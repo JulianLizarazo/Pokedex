f59b9f44da8758c074857420f7c14268ed003182
Julian Lizarazo <julianca20122001@hotmail.com>
2021-08-13 00:07:43 -0500

    pokemon search working, but there is a bug to fix (verify of the hour working succesfully

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,29 @@
 
-function perra() {
-  const ingreso = document.getElementById("ingreso").value;
-  const hola = new Pokemon(ingreso);
+function searchPokemon() {
+  const nameOrIdIngresed = document.getElementById("ingreso").value;
+  const pokemon = new Pokemon(nameOrIdIngresed);
 
-  hola.showPokemonId();
-  hola.showPokemonName();
-  hola.showPokemonAbilities();
-  hola.showPokemonSprite();
-  hola.showPokemonTypes();
-  hola.showPokemonStats();
+  pokemon.showPokemonId();
+  pokemon.showPokemonName();
+  pokemon.showPokemonAbilities();
+  pokemon.showPokemonSprite();
+  pokemon.showPokemonTypes();
+  pokemon.showPokemonStats();
 }
 
+function verifyHour(){
+  const stylesInCommon = 'background-repeat: no-repeat; background-position: center center; background-attachment: fixed; background-size: cover;';
+  const body = document.getElementById("body");
+  const time = new Date();
 
+  if(time.getHours() < 18 && time.getHours() > 6){
+    body.setAttribute("style", 'background-image: url("/Pokedex/assets/imgDay.jpg");' + stylesInCommon);
+  }else{
+    body.setAttribute("style", 'background-image: url("/Pokedex/assets/imgNight.jpg");' + stylesInCommon)
+  }
+}
+
+verifyHour();
 
 
 
--- src/js/js/index.dev.js
+++ src/js/js/index.dev.js
@@ -1,12 +1,26 @@
 "use strict";
 
-function perra() {
-  var ingreso = document.getElementById("ingreso").value;
-  var hola = new Pokemon(ingreso);
-  hola.showPokemonId();
-  hola.showPokemonName();
-  hola.showPokemonAbilities();
-  hola.showPokemonSprite();
-  hola.showPokemonTypes();
-  hola.showPokemonStats();
+function searchPokemon() {
+  var nameOrIdIngresed = document.getElementById("ingreso").value;
+  var pokemon = new Pokemon(nameOrIdIngresed);
+  pokemon.showPokemonId();
+  pokemon.showPokemonName();
+  pokemon.showPokemonAbilities();
+  pokemon.showPokemonSprite();
+  pokemon.showPokemonTypes();
+  pokemon.showPokemonStats();
 }
+
+function verifyHour() {
+  var stylesInCommon = 'background-repeat: no-repeat; background-position: center center; background-attachment: fixed; background-size: cover;';
+  var body = document.getElementById("body");
+  var time = new Date();
+
+  if (time.getHours() < 18 && time.getHours() > 6) {
+    body.setAttribute("style", 'background-image: url("/Pokedex/assets/imgDay.jpg");' + stylesInCommon);
+  } else {
+    body.setAttribute("style", 'background-image: url("/Pokedex/assets/imgNight.jpg");' + stylesInCommon);
+  }
+}
+
+verifyHour();
